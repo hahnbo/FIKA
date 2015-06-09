850a222f6ca539b3cf0d1d46c07bf7a18c627f59
Emanuel Hahn <emanuel.hahn@stern.nyu.edu>
2015-06-09 19:13:16 -0400

    Initial Commit

---
+++ FIKA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIKA
+//
+//  Created by Emanuel Hahn on 6/9/15.
+//  Copyright (c) 2015 Emanuel Hahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
